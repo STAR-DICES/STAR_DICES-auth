fcfcc2ec5e984207de9af4d508ea42771805046c
Alepacox <pacini.alessandro1@gmail.com>
2019-11-16 01:35:03 +0100

    Added flakon base to microservice

--- auth/views/auth.py
+++ auth/views/auth.py
@@ -1,30 +1,34 @@
-from flask import Blueprint, render_template, redirect, request, url_for
+from flakon import SwaggerBlueprint
+from flask import request, jsonify, abort
 from sqlalchemy.exc import IntegrityError
 from auth.database import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
 
-auth = Blueprint('auth', __name__)
+auth = SwaggerBlueprint('auth', __name__, swagger_spec='./auth/views/auth-specs.yaml')
 
 """
 This route is used to display the form to let the user login.
 """
-@auth.route('/login', methods=['GET', 'POST'])
-def login(message=''):
+@auth.operation('login')
+def login(body):
     #To be redirect directly from API Gateway
     #if not current_user.is_anonymous:
     #    return redirect("/", code=302)
-    form = LoginForm()
-    form.message = message
-    if form.validate_on_submit():
-        email, password = form.data['email'], form.data['password']
-        q = db.session.query(User).filter(User.email == email)
-        user = q.first()
-        if user is not None:
-            #Return user id
-            return redirect('/')
-        else:
-            #return 403
-            form.message = "User or Password not correct!"
-    return render_template('login.html', form=form, notlogged=True)
+    print(body)
+    q = db.session.query(User).filter(User.email == body.email)
+    user = q.first()
+    if user is not None and user.authenticate(body.password):
+        return jsonify({'id': user.id})
+    else:
+        return abort(403)
+
+def authenticate(self, password):
+        checked = check_password_hash(self.password, password)
+        self._authenticated = checked
+        return self._authenticated
+
+def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
 
 """
 This route is used to let the user logout.
@@ -38,9 +42,9 @@ This route is used to let the user logout.
 
 """
 This route is used to let a new user signup.
-"""
-@auth.route('/signup', methods=['GET', 'POST'])
-def create_user():
+
+@quizzes.operation('signup')
+def create_user(firstname, lastname, email, dateofbirth, password):
     #To be redirect directly from API Gateway
     #if not current_user.is_anonymous:
     #    return redirect("/", code=302)
@@ -58,3 +62,5 @@ def create_user():
             form.message="Seems like this email is already used"
             
     return render_template('create_user.html', form=form, notlogged=True)
+
+"""
