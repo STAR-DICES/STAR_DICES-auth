6c1f81113efb91afb5792b77f54bdc13335e3737
Stefano Duo <duostefano93@gmail.com>
2019-11-27 03:31:15 +0100

    Fix tests

--- auth/views/test/test_auth.py
+++ auth/views/test/test_auth.py
@@ -6,10 +6,9 @@ from flask_testing import TestCase
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash
 from auth.database import db, User
-from flask_login import current_user
 
 
-class TestHelper(TestCase): # pragma: no cover
+class TestAuth(TestCase): # pragma: no cover
     def create_app(self):
         self.app = start(test=True)
         self.context = self.app.app_context()
@@ -20,51 +19,27 @@ class TestHelper(TestCase): # pragma: no cover
         with self.context:
             db.drop_all()
 
-    def _login(self, email, password, follow_redirects=False):
-        return self.client.post('/login', follow_redirects=follow_redirects, data={
-            'email': email,
-            'password': password
-        })
-
-    def _signup(self, email, password, first_name, last_name, birthday, follow_redirects=False):
-        return self.client.post('/signup', follow_redirects=follow_redirects, data={
-            'email': email,
-            'firstname': first_name,
-            'lastname': last_name,
-            'password': password,
-            'dateofbirth': birthday
-        })
-    
-
-
     def test_login_invalid(self):
-
         # error: invalid request
-        # 
         data = { 'email': "example@example.com" }
-        headers = {'Content-type': 'application/json'}
-        reply = self.client.post('/login', json=data, headers=headers)
+        reply = self.client.post('/login', json=data)
         self.assertEqual(reply.status_code, 400)
 
     def test_login_wrong(self):
         # error: wrong pass
-        # 
         data = { 'email': "example@example.com", 'password': "42" }
-        headers = {'Content-type': 'application/json'}
-        reply = self.client.post('/login', json=data, headers=headers)
+        reply = self.client.post('/login', json=data)
         self.assertEqual(reply.status_code, 401)
 
     def test_login_correct(self):
         # sussess: user and pwd correct
-        # 
         data = { 
                 'email': "example@example.com",
                 'password': "admin"
                 }
-        headers = {'Content-type': 'application/json'}
-        reply = self.client.post('/login', json=data, headers=headers)
+        reply = self.client.post('/login', json=data)
         self.assertEqual(reply.status_code, 200)
-        self.assertEqual(reply.data, { "firstname": "Admin", "user_id": 1 } )
+        self.assertEqual(json.loads(reply.data), { "firstname": "Admin", "user_id": 1 } )
 
         
 
@@ -78,8 +53,7 @@ class TestHelper(TestCase): # pragma: no cover
                 'password': "giacobbe",
                 'dateofbirth': "10/05/2020"
                 }
-        headers = {'Content-type': 'application/json'}
-        reply = self.client.post('/signup', json=data, headers=headers)
+        reply = self.client.post('/signup', json=data)
         self.assertEqual(reply.status_code, 400)
 
     def test_signup_error(self):
@@ -92,8 +66,7 @@ class TestHelper(TestCase): # pragma: no cover
                 'password': "giacobbe",
                 'dateofbirth': "10/05/2020"
                 }
-        headers = {'Content-type': 'application/json'}
-        reply = self.client.post('/signup', json=data, headers=headers)
+        reply = self.client.post('/signup', json=data)
         self.assertEqual(reply.status_code, 409)
 
     def test_signup_success(self):
@@ -106,11 +79,10 @@ class TestHelper(TestCase): # pragma: no cover
                 'password': "giacobbe",
                 'dateofbirth': "01/04/2006"
                 }
-        headers = {'Content-type': 'application/json'}
-        reply = self.client.post('/signup', json=data, headers=headers)
+        reply = self.client.post('/signup', json=data)
         self.assertEqual(reply.status_code, 200)
-        self.assertEqual(reply.data, {
-                                        'user_id': 2,
+        self.assertEqual(json.loads(reply.data), {
+                                        'user_id': 3,
                                         'firstname': "holy10characterspls"
                                         })
 
